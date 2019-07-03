ddca4ba76054b61b3982503c147834875a66e96c
Preston Van Loon <preston@prysmaticlabs.com>
2019-07-03 11:52:02 -0400

    revert to v0.7.1 ssz

--- test_libs/pyspec/eth2spec/utils/ssz/ssz_impl.py
+++ test_libs/pyspec/eth2spec/utils/ssz/ssz_impl.py
@@ -1,8 +1,11 @@
-from ..merkle_minimal import merkleize_chunks
-from ..hash_function import hash
-from .ssz_typing import (
-    SSZValue, SSZType, BasicValue, BasicType, Series, Elements, Bits, boolean, Container, List, Bytes,
-    Bitlist, Bitvector, uint,
+from ..merkle_minimal import merkleize_chunks, hash
+from eth2spec.utils.ssz.ssz_typing import (
+    is_uint_type, is_bool_type, is_container_type,
+    is_list_kind, is_vector_kind,
+    read_vector_elem_type, read_elem_type,
+    uint_byte_size,
+    infer_input_type,
+    get_zero_value,
 )
 
 # SSZ Serialization
@@ -11,53 +14,68 @@ from .ssz_typing import (
 BYTES_PER_LENGTH_OFFSET = 4
 
 
-def serialize_basic(value: SSZValue):
-    if isinstance(value, uint):
-        return value.to_bytes(value.type().byte_len, 'little')
-    elif isinstance(value, boolean):
+def is_basic_type(typ):
+    return is_uint_type(typ) or is_bool_type(typ)
+
+
+def serialize_basic(value, typ):
+    if is_uint_type(typ):
+        return value.to_bytes(uint_byte_size(typ), 'little')
+    elif is_bool_type(typ):
         if value:
             return b'\x01'
         else:
             return b'\x00'
     else:
-        raise Exception(f"Type not supported: {type(value)}")
+        raise Exception("Type not supported: {}".format(typ))
 
 
-def deserialize_basic(value, typ: BasicType):
-    if issubclass(typ, uint):
+def deserialize_basic(value, typ):
+    if is_uint_type(typ):
         return typ(int.from_bytes(value, 'little'))
-    elif issubclass(typ, boolean):
+    elif is_bool_type(typ):
         assert value in (b'\x00', b'\x01')
-        return typ(value == b'\x01')
+        return True if value == b'\x01' else False
     else:
-        raise Exception(f"Type not supported: {typ}")
+        raise Exception("Type not supported: {}".format(typ))
 
 
-def is_empty(obj: SSZValue):
-    return type(obj).default() == obj
-
-
-def serialize(obj: SSZValue):
-    if isinstance(obj, BasicValue):
-        return serialize_basic(obj)
-    elif isinstance(obj, Bitvector):
-        as_integer = sum([obj[i] << i for i in range(len(obj))])
-        return as_integer.to_bytes((len(obj) + 7) // 8, "little")
-    elif isinstance(obj, Bitlist):
-        as_integer = sum([obj[i] << i for i in range(len(obj))])
-        return as_integer.to_bytes((len(obj) + 7) // 8, "little")
-    elif isinstance(obj, Series):
-        return encode_series(obj)
+def is_fixed_size(typ):
+    if is_basic_type(typ):
+        return True
+    elif is_list_kind(typ):
+        return False
+    elif is_vector_kind(typ):
+        return is_fixed_size(read_vector_elem_type(typ))
+    elif is_container_type(typ):
+        return all(is_fixed_size(t) for t in typ.get_field_types())
     else:
-        raise Exception(f"Type not supported: {type(obj)}")
+        raise Exception("Type not supported: {}".format(typ))
 
 
-def encode_series(values: Series):
-    if isinstance(values, bytes):  # Bytes and BytesN are already like serialized output
+def is_empty(obj):
+    return get_zero_value(type(obj)) == obj
+
+
+@infer_input_type
+def serialize(obj, typ=None):
+    if is_basic_type(typ):
+        return serialize_basic(obj, typ)
+    elif is_list_kind(typ) or is_vector_kind(typ):
+        return encode_series(obj, [read_elem_type(typ)] * len(obj))
+    elif is_container_type(typ):
+        return encode_series(obj.get_field_values(), typ.get_field_types())
+    else:
+        raise Exception("Type not supported: {}".format(typ))
+
+
+def encode_series(values, types):
+    # bytes and bytesN are already in the right format.
+    if isinstance(values, bytes):
         return values
 
     # Recursively serialize
-    parts = [(v.type().is_fixed_size(), serialize(v)) for v in values]
+    parts = [(is_fixed_size(types[i]), serialize(values[i], typ=types[i])) for i in range(len(values))]
 
     # Compute and check lengths
     fixed_lengths = [len(serialized) if constant_size else BYTES_PER_LENGTH_OFFSET
@@ -89,16 +107,10 @@ def encode_series(values: Series):
 # -----------------------------
 
 
-def pack(values: Series):
-    if isinstance(values, bytes):  # Bytes and BytesN are already packed
+def pack(values, subtype):
+    if isinstance(values, bytes):
         return values
-    elif isinstance(values, Bitvector):
-        as_integer = sum([values[i] << i for i in range(len(values))])
-        return as_integer.to_bytes((values.length + 7) // 8, "little")
-    elif isinstance(values, Bitlist):
-        as_integer = sum([values[i] << i for i in range(len(values))])
-        return as_integer.to_bytes((values.length + 7) // 8, "little")
-    return b''.join([serialize_basic(value) for value in values])
+    return b''.join([serialize_basic(value, subtype) for value in values])
 
 
 def chunkify(bytez):
@@ -111,52 +123,41 @@ def mix_in_length(root, length):
     return hash(root + length.to_bytes(32, 'little'))
 
 
-def is_bottom_layer_kind(typ: SSZType):
+def is_bottom_layer_kind(typ):
     return (
-        isinstance(typ, BasicType) or
-        (issubclass(typ, Elements) and isinstance(typ.elem_type, BasicType))
+        is_basic_type(typ) or
+        (is_list_kind(typ) or is_vector_kind(typ)) and is_basic_type(read_elem_type(typ))
     )
 
 
-def item_length(typ: SSZType) -> int:
-    if issubclass(typ, BasicValue):
-        return typ.byte_len
+@infer_input_type
+def get_typed_values(obj, typ=None):
+    if is_container_type(typ):
+        return obj.get_typed_values()
+    elif is_list_kind(typ) or is_vector_kind(typ):
+        elem_type = read_elem_type(typ)
+        return list(zip(obj, [elem_type] * len(obj)))
     else:
-        return 32
+        raise Exception("Invalid type")
 
 
-def chunk_count(typ: SSZType) -> int:
-    if isinstance(typ, BasicType):
-        return 1
-    elif issubclass(typ, Bits):
-        return (typ.length + 255) // 256
-    elif issubclass(typ, Elements):
-        return (typ.length * item_length(typ.elem_type) + 31) // 32
-    elif issubclass(typ, Container):
-        return len(typ.get_fields())
+@infer_input_type
+def hash_tree_root(obj, typ=None):
+    if is_bottom_layer_kind(typ):
+        data = serialize_basic(obj, typ) if is_basic_type(typ) else pack(obj, read_elem_type(typ))
+        leaves = chunkify(data)
     else:
-        raise Exception(f"Type not supported: {typ}")
-
-
-def hash_tree_root(obj: SSZValue):
-    if isinstance(obj, Series):
-        if is_bottom_layer_kind(obj.type()):
-            leaves = chunkify(pack(obj))
-        else:
-            leaves = [hash_tree_root(value) for value in obj]
-    elif isinstance(obj, BasicValue):
-        leaves = chunkify(serialize_basic(obj))
-    else:
-        raise Exception(f"Type not supported: {type(obj)}")
-
-    if isinstance(obj, (List, Bytes, Bitlist)):
-        return mix_in_length(merkleize_chunks(leaves, pad_to=chunk_count(obj.type())), len(obj))
+        fields = get_typed_values(obj, typ=typ)
+        leaves = [hash_tree_root(field_value, typ=field_typ) for field_value, field_typ in fields]
+    if is_list_kind(typ):
+        return mix_in_length(merkleize_chunks(leaves), len(obj))
     else:
         return merkleize_chunks(leaves)
 
 
-def signing_root(obj: Container):
+@infer_input_type
+def signing_root(obj, typ):
+    assert is_container_type(typ)
     # ignore last field
-    fields = [field for field in obj][:-1]
-    leaves = [hash_tree_root(f) for f in fields]
+    leaves = [hash_tree_root(field_value, typ=field_typ) for field_value, field_typ in obj.get_typed_values()[:-1]]
     return merkleize_chunks(chunkify(b''.join(leaves)))
